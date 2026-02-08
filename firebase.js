ebe35c63665aa0b10e16e1a79a6cee40c5384b95
kypadmission <csc572767650016@gmail.com>
2026-02-08 15:46:16 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,15 @@
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"></script>
+
+<script>
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "kyp-admission-portal.firebaseapp.com",
+  projectId: "kyp-admission-portal",
+  storageBucket: "kyp-admission-portal.appspot.com",
+  messagingSenderId: "XXXXXXX",
+  appId: "XXXXXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
+</script>
