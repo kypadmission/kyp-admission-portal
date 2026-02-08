680e131eec44de76e5164dd936f7508e9f35fdef
kypadmission <csc572767650016@gmail.com>
2026-02-08 16:49:48 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,18 @@
-<!-- Firebase SDKs -->
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js"></script>
-<script src="https://www.gstatic.com/firebasejs/9.23.0/firebase-auth-compat.js"></script>
+<script type="module">
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-<script>
-  // Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCpeujBjXSBgEntW_tZ7GaGRxOauub5BLA",
-    authDomain: "kyp-admission-portal.firebaseapp.com",
-    projectId: "kyp-admission-portal",
-    storageBucket: "kyp-admission-portal.appspot.com",
-    messagingSenderId: "696364940629",
-    appId: "1:696364940629:web:b6e7a5adeedcb439fcc3dc",
-    measurementId: "G-QRN13QK1R3"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCpeujBjXSBgEntW_tZ7GaGRxOauub5BLA",
+  authDomain: "kyp-admission-portal.firebaseapp.com",
+  projectId: "kyp-admission-portal",
+  storageBucket: "kyp-admission-portal.appspot.com",
+  messagingSenderId: "696364940629",
+  appId: "1:696364940629:web:b6e7a5adeedcb439fcc3dc"
+};
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 </script>
